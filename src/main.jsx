703f68f6d182f53cf1a5a120838a3c71fcc2ce64
Abhishek Kumar <abhishek8579013@gmail.com>
2025-07-11 13:55:05 +0530

    m

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App'; // ✅ Import your App component
-import "../src/app.css"
+// import "../src/app.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
